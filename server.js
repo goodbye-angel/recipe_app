7aff1af55feb23ffb813dcb681c5fee4dc8b3cbf
Peter <ptk726@gmail.com>
2017-12-14 11:17:21 -0500

    prints hello world

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express'),
+    mongoose = require('mongoose'),
+    port = 3000 || process.env.PORT,
+    app = express(),
+    db = mongoose.connection;
+
+mongoose.Promise = global.Promise;
+const mongoURI = 'mongodb://localhost/recipe_app';
+
+// Connect to Mongo
+mongoose.connect(mongoURI, { useMongoClient: true });
+
+// Error / success
+db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
+db.on('connected', () => console.log('mongo connected: ', mongoURI));
+db.on('disconnected', () => console.log('mongo disconnected'));
+
+
+// open the connection to mongo
+db.on('open', () => {
+    console.log('Connection made!');
+});
+
+app.use(express.urlencoded({ extended: false }));// extended: false - does not allow nested objects in query strings
+app.use(express.json());// returns middleware that only parses JSON
+
+
+app.get('/', function (req, res) {
+    res.send('Hello world! Here is our recipe app!')
+});
+
+app.use(express.static('public'));
+
+app.listen(port, () => {
+    console.log('=======================');
+    console.log('Running on port ' + port);
+    console.log('=======================');
+});
+
+
