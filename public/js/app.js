4b32552f95d59da15089c88df28bbbc91a55de31
Peter <ptk726@gmail.com>
2017-12-14 21:46:33 -0500

    updated index app css files

--- public/js/app.js
+++ public/js/app.js
@@ -1,57 +1,88 @@
-const app = angular.module('worldbank', []);
+const app = angular.module('RecipesApp', []);
+
+app.controller('MainController', ['$http', function ($http) {
+
+    this.createForm = {};
+
+    this.recipe = '';
 
 
-app.controller('mainController', ['$http', function ($http) {
-    //stuff
-
-    this.recipes = [];
-    this.newRecipe = {};
-
-    //click on region name and newRecipe store in global variable
-    this.clickFornewRecipes = (id) => {
-        console.log()
-        $http({
-            method: 'GET',
-            url: '/recipes/' + id
-        }).then(response => {
-            this.newRecipe = response.data;
-            console.log(this.newRecipe);
-        }, error => {
-            console.error(error.message);
-        }).catch(err => console.error('Catch: ', err));
-    }
-
-    //load immediately on page load 
-    this.clickFornewRecipes();
-
-
-
-    this.uniqueRegions = () => {
-        $http({
-            method: 'GET',
-            url: '/recipes/uniqueRegions'
-        }).then(response => {
-            this.regions = response.data;
-            
-        }, error => {
-            console.error(error.message);
-        }).catch(err => console.error('Catch: ', err));
-    }
-    //load immediately on page load 
-    this.uniqueRegions();
-
-
-
-    
     this.createRecipe = () => {
         $http({
             method: 'POST',
             url: '/recipes',
-            data: this.newRecipe
+            data: this.createForm
         }).then(response => {
             this.recipes.push(response.data);
-            console.log(response.data);
-            // this.newRecipe = {};
+            this.createForm = {};
+        }, error => {
+            console.error(error.message);
+        }).catch(err => console.error('Catch: ', err));
+    }
+
+    this.getRecipes = () => {
+        $http({
+            method: 'GET',
+            url: '/recipes'
+        }).then(response => {
+            this.recipes = response.data;
+            this.recipe = this.recipes[0];
+        }, error => {
+            console.error(error.message);
+        }).catch(err => console.error('Catch: ', err));
+    }
+
+    //load immediately on page load
+    this.getRecipes();
+
+    this.deleteRecipe = (id) => {
+        console.log("I'm going to delete you", id);
+    }
+
+    this.deleteRecipe = (id) => {
+        $http({
+            method: 'DELETE',
+            url: '/recipes/' + id
+        }).then(response => {
+            const removeByIndex = this.recipes.findIndex(recipe => recipe._id === id)
+            this.recipes.splice(removeByIndex, 1);
+        }
+            , error => {
+                console.error(error.message);
+            }).catch(err => console.error('Catch: ', err));
+    }
+
+    this.updateCelebrated = (recipe) => {
+
+        recipe.celebrated = !recipe.celebrated;
+
+        $http({
+            method: 'PUT',
+            url: '/recipes/' + recipe._id,
+            data: { celebrated: recipe.celebrated }
+        }).then(response => {
+            console.log(response.data.celebrated);
+        }, error => {
+            console.error(error.message);
+        }).catch(err => console.error('Catch: ', err));
+    }
+
+
+    this.chooseOneRecipe = (recipe) => {
+        this.recipe = recipe;
+        console.log(this.recipe.name);
+    }
+
+
+    // add increment likes
+    this.addRecipe = (recipe) => {
+
+        $http({
+            method: 'PUT',
+            url: '/recipes/' + recipe._id,
+            data: { likes: recipe.likes }
+        }).then(response => {
+            console.log(response.data.likes);
         }, error => {
             console.error(error.message);
         }).catch(err => console.error('Catch: ', err));
@@ -59,7 +90,4 @@ app.controller('mainController', ['$http', function ($http) {
 
 
 
-
-
-
-}]);
+}]); //this closes app.controller
